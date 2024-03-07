14193a2dfe6f0907f9cebd3b452c6201703b83cf
koyel222 <koyelrakshit222@gmail.com>
2024-03-07 12:25:19 +0530

    first

---
+++ order.js
@@ -0,0 +1,26 @@
+const Orders=[
+    {
+    productName:'Foldable Mini Drone',
+    productNumber:'85631',
+    paymentStatus:'Due',
+    shipping:'Pending'
+    },
+    {
+        productName:'LARVENDER KF102 Drone',
+        productNumber:'36378',
+        paymentStatus:'Refunded',
+        shipping:'Declined'
+        },
+        {
+            productName:'Ruko F11 pro Drone',
+            productNumber:'49347',
+            paymentStatus:'Due',
+            shipping:'Pending'
+            },
+            {
+                productName:'Drone with camera drone',
+                productNumber:'85631',
+                paymentStatus:'Paid',
+                shipping:'Delivered'
+                }
+]
